07385360b831fbcda1c624b45dbd244b08a05bbd
Macallister Crane <macallistercrane@yahoo.com>
2024-08-12 22:46:36 +0000

    Uploaded BMI calculator

---
+++ bmi.py
@@ -0,0 +1,15 @@
+height = int(input("Enter your height in M: "))
+weight = int(input("Enter your weight in Kg: "))
+
+bmi = round(weight / height ** 2)
+
+if bmi < 18.5:
+    print("You are underweight with a BMI of", bmi)
+elif bmi < 25:
+    print("You are in the normal weight range with a BMI of", bmi)
+elif bmi < 30:
+    print("You are overweight", bmi)
+elif bmi < 35:
+    print("You are obese you hog", bmi)
+else:
+    print("You are clinically obese you fat piece of shit", bmi)
